748cacfcd58c467a64c1a6c1ed6db2f78e840caf
James Bailey <jbailey@codisms.com>
2018-06-26 00:10:25 +0000

    Add test files for language server

---
+++ lang-serv-test/javascript/lib.js
@@ -0,0 +1,11 @@
+
+export class Whee {
+	negative(x) {
+		return -x;
+	}
+
+	multiply(x, y) {
+		return x * y;
+	}
+}
+
---
+++ lang-serv-test/javascript/test.js
@@ -0,0 +1,22 @@
+
+import { Whee } from './lib';
+
+class Foo {
+	foobar(message, options) {
+		console.log(message, options);
+	}
+
+	foobarlong() {
+	}
+
+	foowhee() {
+	}
+
+	foobarshort() {
+	}
+}
+
+var w = new Whee();
+
+var f = new Foo();
+
