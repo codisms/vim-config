ffc5a5516d81ac1bf77c763226967d39dbc5977c
James Bailey <jbailey@codisms.com>
2023-05-19 23:50:11 -0700

    coc settings and install; remove fzf

--- config-vim.sh
+++ config-vim.sh
@@ -45,59 +45,8 @@ configureVim() {
 	cd ..
 }
 
-installVimExtensions_YCM() {
-	echo -e "\e[35mInstalling YouCompleteMe...\e[0m"
-
-	cd ${HOME}/.vim/pack/bundle/start/YouCompleteMe
-	#./install.py echo "~~~ PATH = ${PATH}"
-
-	# https://unix.stackexchange.com/a/233287 FREE_MEMORY=$(free | awk -v RS="" '{ print $10 / 1024; }'
-	#| bc)
-	FREE_MEMORY=$(cat /proc/meminfo | grep -e '\(Swap\|Mem\)Free' | awk -v RS="" '{ print $2 + $5; }' |
-bc)
-	echo FREE_MEMORY = ${FREE_MEMORY}
-	if [ $FREE_MEMORY -lt 1048576 ]; then
-		echo -e "\e[31;5mUsing low-memory compilation options\e[0m"
-		# We're going to assume a large enough swap has been specified
-
-		YCM_CORES=1 ./install.py --clang-completer --gocode-completer --tern-completer
-	else
-		echo "Using normal compilation options"
-		./install.py --clang-completer --gocode-completer --tern-completer
-	fi
-
-	#./install.py --clang-completer --system-libclang --gocode-completer > /dev/null
-}
-
-installVimExtensions_FZF() {
-	echo -e "\e[35mInstalling fzf...\e[0m"
-
-	${HOME}/.vim/pack/bundle/start/fzf/install --bin
-	if ! grep -q fzf ${HOME}/.profile; then
-		echo "export PATH=\$PATH:${HOME}/.vim/pack/bundle/start/fzf/bin" >> ${HOME}/.profile
-	fi
-}
-
-installVimExtensions_LangServ() {
-	echo -e "\e[35mInstalling Javascript/Typescript language server...\e[0m"
-	npm install -g javascript-typescript-langserver
-
-	echo -e "\e[35mInstalling CSS/LESS/SASS language server...\e[0m"
-	npm install -g vscode-css-languageserver-bin
-
-	echo -e "\e[35mInstalling Go language server...\e[0m"
-	go install golang.org/x/tools/gopls@latest
-
-	echo -e "\e[35mInstalling Python language server...\e[0m"
-	pip3 install 'python-language-server[all]'
-}
-
 installVimExtensions() {
 	echo -e "\e[36mInstalling vim extensions\e[0m"
-
-	#installVimExtensions_YCM
-	installVimExtensions_FZF
-	installVimExtensions_LangServ
 
 	echo -e "\e[35mInstalling vim-polyglot...\e[0m"
 	if [ -f ${HOME}/.vim/pack/bundle/start/vim-polyglot/build ]; then
@@ -114,7 +63,14 @@ installVimExtensions() {
 	npm install -g prettier
 
 	echo -e "\e[35mInstalling coc extensions...\e[0m"
-	vim -c 'CocInstall -sync coc-angular coc-html coc-css coc-eslint coc-go coc-json coc-java coc-python coc-sql coc-tsserver|q'
+	mkdir -p ~/.config/coc/extensions
+	cd ~/.config/coc/extensions
+	if [ ! -f package.json ]; then
+		echo '{"dependencies":{}}'> package.json
+	fi
+	npm install --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod \
+		coc-angular coc-css coc-docker coc-eslint coc-go coc-html coc-java coc-json coc-prettier \
+		@yaegassy/coc-pylsp coc-sql coc-tsserver
 }
 
 if [ ${BUILD} -eq 1 ]; then
